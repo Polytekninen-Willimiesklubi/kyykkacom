18491e84247fe4b01d8d48f796e746b8dede8fc6
Totti-S <totti.sillanpaa@gmail.com>
2023-12-08 18:47:39 +0200

    'PlayerView' added

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -72,6 +72,16 @@ export default new Router({
             component: function () {
                 return import(/* webpackChunkName: "pelaajat" */ './views/JoukkueView.vue');
             }
+        },
+        {
+            path: '/pelaaja/(.*)',
+            name: 'pelaaja',
+            // route level code-splitting
+            // this generates a separate chunk (about.[hash].js) for this route
+            // which is lazy-loaded when the route is visited.
+            component: function () {
+                return import(/* webpackChunkName: "pelaajat" */ './views/PelaajaView.vue');
+            }
         }
     ]
 });
---
+++ frontend/src/views/PelaajaView.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-layout>
+      <v-flex auto>
+        <pelaaja></pelaaja>
+      </v-flex>
+      <v-flex xs4 class="pl-3 hidden-md-and-down">
+        <side-bar></side-bar>
+      </v-flex>
+    </v-layout>
+  </template>
+
+
+<script>
+import SideBar from '@/components/SideBar';
+import Pelaaja from '@/components/Pelaaja';
+
+export default {
+    name: 'pelaajaview',
+    components: {
+        Pelaaja,
+        SideBar
+    }
+};
+</script>
