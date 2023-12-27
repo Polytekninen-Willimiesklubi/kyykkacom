40645573cb702a9f9e09953eda08659963b6577c
Totti Sillanpää <totti.sillanpaa@magister.fi>
2023-12-27 11:59:16 +0200

    Updated import matches with test function

--- utils/import_matches.py
+++ utils/import_matches.py
@@ -3,8 +3,22 @@
 
 import csv, datetime
 from django.db import transaction
-from kyykka.models import Team, Match, CurrentSeason
+from kyykka.models import Team, Match, CurrentSeason, TeamsInSeason
 
+def test_import():
+    c = CurrentSeason.objects.first()
+    print(f"Current season is set to: {CurrentSeason.objects.first()}")
+    print(f'First matches binds are: ')
+    with open('./matches.csv') as csvfile:
+        i = 0
+        readCSV = csv.reader(csvfile, delimiter=',')
+        with transaction.atomic():
+            for row in readCSV:
+                if i == 4: break
+                home_team = TeamsInSeason.objects.get(current_abbreviation=row[1], season=c.season)
+                away_team = TeamsInSeason.objects.get(current_abbreviation=row[2], season=c.season)
+                print(row[1], '->', home_team, f'ID: {home_team.id}, Season: {home_team.season}')
+                print(row[2], '->', away_team, f'ID: {away_team.id}, Season: {away_team.season}')
 
 def import_matches():
     """
@@ -12,6 +26,7 @@ def import_matches():
     """
     with open('./matches.csv') as csvfile:
         readCSV = csv.reader(csvfile, delimiter=',')
+        current = CurrentSeason.objects.first().season
         with transaction.atomic():
             for row in readCSV:
                 date_time_str = row[0].replace("'", "")
@@ -19,11 +34,11 @@ def import_matches():
                 away = row[2]
                 field = row[3]
                 date_time_obj = datetime.datetime.strptime(date_time_str, '%Y-%m-%d %H:%M:%S')
-                home_team = Team.objects.get(abbreviation=home)
-                away_team = Team.objects.get(abbreviation=away)
+                home_team = TeamsInSeason.objects.get(current_abbreviation=home, season=current)
+                away_team = TeamsInSeason.objects.get(current_abbreviation=away, season=current)
                 field_num = field[-1]
                 Match.objects.create(
-                    season=CurrentSeason.objects.first().season,
+                    season=current,
                     match_time=date_time_obj,
                     field=field_num,
                     home_team=home_team,
