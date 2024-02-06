5dbc2b6176a2f48bcc35a97228e7d2cf03f2ac33
Totti-S <totti.sillanpaa@gmail.com>
2024-02-06 18:37:37 +0200

    Same update again: fixed url

--- api/urls.py
+++ api/urls.py
@@ -38,7 +38,7 @@ urlpatterns = [
     path('throws/update/<int:pk>/', views.ThrowAPI.as_view()),
     path('seasons',  views.SeasonsAPI.as_view()),
     path('superweekend/', views.SuperWeekendAPI.as_view(), name='superweekend'),
-    path('kyykka_admin', views.KyykkaAdminViewSet.as_view(), name='teams-in-season')
+    path('kyykka_admin/team/update/<int:pk>', views.KyykkaAdminViewSet.as_view(), name='teams-in-season')
 ]
 
 router = SimpleRouter()
