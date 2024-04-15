dbdcb87584ce3c5b44bb25ddea5f9802c359bdea
Totti-S <totti.sillanpaa@gmail.com>
2024-04-15 20:37:23 +0300

    Removed Old main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,19 +1,3 @@
-// import { createApp } from 'vue'
-// import { createPinia } from 'pinia'
-// import { router } from './router'
-// import vuetify from './plugins/vuetify'
-// import App from './App.vue'
-
-
-// const app = createApp(App)
-
-// app.use(router)
-// app.use(vuetify)
-// app.use(createPinia())
-// app.use(createVuetify)
-
-// app.mount('#app')
-
 /**
  * main.js
  *
