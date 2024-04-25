57a1c71692766c539824dac488a75b2e56744a05
Totti-S <totti.sillanpaa@gmail.com>
2024-04-25 15:51:02 +0300

    Refactored torunament component

--- frontend/src/components/Tournament.vue
+++ frontend/src/components/Tournament.vue
@@ -34,13 +34,11 @@
   </v-layout>
 </template>
 
-<script>
+<script setup>
 import Bracket from 'vue-tournament-bracket'
 
-export default {
-  name: 'Tournament',
-  props: {
-    type: Number,
+const props = defineProps({
+  type: Number,
     played_games: Array,
     rounds_parrent: Array,
     first_round: Boolean,
@@ -53,184 +51,177 @@ export default {
       type: Boolean,
       default: true
     }
-  },
-  components: {
-    Bracket
-  },
-  data () {
-    return {
-      tmp_rounds: [{}, {}, {}, {}, {}, {}],
-      rounds: [],
-      data: [],
-      bracket_matches: [],
-      st_round: [],
-      loaded_undefined: false
-    }
-  },
-  methods: {
-    putTeamsPlayoffBracket () {
-      if (this.non_default_seeds !== undefined) {
-        this.non_default_seeds.forEach(ele => {
-          if (ele[1] !== undefined) {
-            for (let matchIdx = 0; matchIdx < this.rounds.length; matchIdx++) {
-              const placementString = (ele[1]).toString() + '. Seed'
-              const match = this.rounds[matchIdx]
-              if (match.player1.name.startsWith(placementString)) {
-                match.player1.name = ele[0]
-                break
-              } else if (match.player2.name.startsWith(placementString)) {
-                match.player2.name = ele[0]
-                break
-              }
-            }
-          }
-        })
-      } else {
-        this.bracket_placements.forEach((bracket, idx) => {
-          bracket.sort((a,b) => a.bracket_placement-b.bracket_placement)
-          for (let i = 0; i < bracket.length; i++) {
-            for (let matchIdx = 0; matchIdx < this.rounds.length; matchIdx++) {
-              const placementString = this.bracket_placements.length >= 2
-                ? String.fromCharCode(65 + idx) + (i + 1).toString()
-                : (i + 1).toString() + '. Seed';
-              const match = this.rounds[matchIdx]
-              if (match.player1.name === placementString) {
-                match.player1.name = bracket[i].current_abbreviation
-                break
-              } else if (match.player2.name === placementString) {
-                match.player2.name = bracket[i].current_abbreviation
-                break
-              }
-            }
-          }
-        })
-      }
-    },
-    resolveGames () {
-      this.bracket_matches = this.played_games.filter(ele => !ele.post_season)
-      const playoff_games = this.played_games.filter(ele => ele.post_season || ele.match_type >= 32)
-      playoff_games.forEach(ele => {
-        ele.match_type = ele.match_type >= 32 ? ele.match_type - 30 : ele.match_type
-      })
-      playoff_games.forEach(ele => {
-        if (ele.match_type >= 2 & ele.match_type < 10) {
-          const round = this.tmp_rounds[ele.match_type - 2]
-          if (ele.seriers in round === false) {
-            round[ele.seriers] = {}
-            round[ele.seriers][ele.home_team.current_abbreviation] = 0
-            round[ele.seriers][ele.away_team.current_abbreviation] = 0
-          }
-          if (ele.home_score_total < ele.away_score_total) {
-            ++round[ele.seriers][ele.home_team.current_abbreviation]
-          } else if (ele.home_score_total > ele.away_score_total) {
-            ++round[ele.seriers][ele.away_team.current_abbreviation]
-          }
-        }
-      })
-    },
-    loadRounds () {
-      if (this.rounds_parrent.length == 0) {
-        this.loaded_undefined = true
-        return
-      }
-      this.rounds = structuredClone(toRaw(this.rounds_parrent))
-      this.rounds.forEach(ele => {
-        ele.player1.template_name = ele.player1.name
-        ele.player2.template_name = ele.player2.name
-      })
-      this.loaded_undefined = false
-    },
-    splitFirstRound () {
-      if (!this.first_round) {
-        this.data = this.rounds
-        return
-      }
-      const first_round_matches = this.rounds.filter(e => e.type == this.first)
-      this.st_round = first_round_matches
-      const games = this.tmp_rounds[this.first - 2]
-      const winners = []
-      for (const [key, el] of Object.entries(games)) {
-        const match = this.st_round.find(e => e.player1.name == Object.keys(el)[0] || e.player2.name == Object.keys(el)[0])
-        if (match === undefined) {
-          console.log("Didn't find correct match. First round +  element: " + Object.keys(el))
-          return
-        }
-        const winner_team = el[match.player1.name] > el[match.player2.name] ? match.player1 : match.player2
-        const loser_team = el[match.player1.name] > el[match.player2.name] ? match.player2 : match.player1
-        const new_winner_team = structuredClone(winner_team)
-        new_winner_team.winner = null
-        winners.push(new_winner_team)
-        winner_team.winner = true
-        loser_team.winner = false
-        match.other_info = el[match.player1.name].toString() + ' - ' + el[match.player2.name].toString()
-      }
-      winners.sort((a, b) => Number(b.id) - Number(a.id))
-      winners.forEach((winner, i) => {
-        const placementString = (i + 1).toString() + '. Low Seed'
-        const new_match = this.rounds.find(el => el.player1.name == placementString || el.player2.name == placementString)
-        const correct_column = new_match.player1.name.includes(placementString) ? 'player1' : 'player2'
-        const template = new_match[correct_column].template_name
-        winner.template_name = template
-        new_match[correct_column] = winner
-      })
-      this.data = this.rounds.filter(e => e.type != this.first)
-    },
-    resolvePlayoffs () {
-      const reversed_list = this.tmp_rounds.reverse()
-      reversed_list.forEach((ele, i) => {
-        for (const [key, el] of Object.entries(ele)) {
-          if (this.first_round && this.first == 7 - i) { continue }
-          const match = this.data.find(e => e.type == 7 - i && (Object.keys(el)[0] == e.player1.name || Object.keys(el)[0] == e.player2.name))
-          if (match === undefined) {
-            console.log("Didn't find correct match. type: " + 7 - i + ' element: ' + Object.keys(el))
-            return
-          }
-          if (el[match.player1.name] !== el[match.player2.name]) { // Not a tie
-            const winner = el[match.player1.name] > el[match.player2.name] ? match.player1 : match.player2
-            const loser = el[match.player1.name] > el[match.player2.name] ? match.player2 : match.player1
-            const new_winner = structuredClone(toRaw(winner))
-            const new_loser = structuredClone(toRaw(loser))
-            new_winner.winner = null
-            new_loser.winner = null
-            winner.winner = true
-            loser.winner = false
-            // console.log(match);
-            // console.log(el);
-            match.other_info = el[match.player1.name].toString() + ' - ' + el[match.player2.name].toString()
-            if (7 - i >= 4) { // Ignore Bronze and Finals
-              if (7 - i == 4) { // SemiFinals -> Winner needs to be assigned to Finals
-                const finals = this.rounds.find(ele => ele.type === 2)
-                const correct_column = finals.player1.name.includes(match.name) ? 'player1' : 'player2'
-                const template = finals[correct_column].template_name
-                new_winner.template_name = template
-                finals[correct_column] = new_winner
-              }
-              const new_match = this.rounds.find(ele => ele.id === match.next)
-              const n = (7 - i != 4) ? 'name' : 'loser_name'
-              const correct_column = new_match.player1.name.includes(match[n]) ? 'player1' : 'player2'
-              const template = new_match[correct_column].template_name
+});
 
-              new_match[correct_column] = (7 - i == 4) && this.bronze ? new_loser : new_winner // SemiFinals -> Loser needs to be assigned to Bronze match
-              new_match[correct_column].template_name = template
-            }
+const tmp_rounds = ref([{}, {}, {}, {}, {}, {}]);
+const rounds = ref([]);
+const data = ref([]);
+const bracket_matches = ref([]);
+const st_round = ref([]);
+const loaded_undefined = ref(false);
+
+function putTeamsPlayoffBracket() {
+  if (props.non_default_seeds !== undefined) {
+    props.non_default_seeds.forEach(ele => {
+      if (ele[1] !== undefined) {
+        for (let matchIdx = 0; matchIdx < rounds.value.length; matchIdx++) {
+          const placementString = (ele[1]).toString() + '. Seed'
+          const match = rounds.value[matchIdx]
+          if (match.player1.name.startsWith(placementString)) {
+            match.player1.name = ele[0]
+            break
+          } else if (match.player2.name.startsWith(placementString)) {
+            match.player2.name = ele[0]
+            break
           }
         }
-      }, this)
-    }
-  },
-  watch: {
-    load_ended () {
-      this.loadRounds()
-      if (!this.loaded_undefined) {
-        this.resolveGames()
-        this.putTeamsPlayoffBracket()
-        this.splitFirstRound()
-        this.resolvePlayoffs()
+      }
+    })
+    return
+  }
+  props.bracket_placements.forEach((bracket, idx) => {
+    bracket.sort((a,b) => a.bracket_placement-b.bracket_placement)
+    for (let i = 0; i < bracket.length; i++) {
+      for (let matchIdx = 0; matchIdx < rounds.value.length; matchIdx++) {
+        const placementString = props.bracket_placements.length >= 2
+          ? String.fromCharCode(65 + idx) + (i + 1).toString()
+          : (i + 1).toString() + '. Seed';
+        const match = rounds.value[matchIdx]
+        if (match.player1.name === placementString) {
+          match.player1.name = bracket[i].current_abbreviation
+          break
+        } else if (match.player2.name === placementString) {
+          match.player2.name = bracket[i].current_abbreviation
+          break
+        }
       }
     }
-  }
+  })
 }
+function resolveGames() {
+  bracket_matches.value = props.played_games.filter(ele => !ele.post_season)
+  const playoff_games = props.played_games.filter(ele => ele.post_season || ele.match_type >= 32)
+  playoff_games.forEach(ele => {
+    ele.match_type = ele.match_type >= 32 ? ele.match_type - 30 : ele.match_type
+  })
+  playoff_games.forEach(ele => {
+    if (ele.match_type >= 2 & ele.match_type < 10) {
+      const round = tmp_rounds.value[ele.match_type - 2]
+      if (!(ele.seriers in round)) {
+        round[ele.seriers] = {}
+        round[ele.seriers][ele.home_team.current_abbreviation] = 0
+        round[ele.seriers][ele.away_team.current_abbreviation] = 0
+      }
+      if (ele.home_score_total < ele.away_score_total) {
+        ++round[ele.seriers][ele.home_team.current_abbreviation]
+      } else if (ele.home_score_total > ele.away_score_total) {
+        ++round[ele.seriers][ele.away_team.current_abbreviation]
+      }
+    }
+  })
+}
+function loadRounds() {
+  if (props.rounds_parrent.length == 0) {
+    loaded_undefined.value = true
+    return
+  }
+  rounds.value = structuredClone(toRaw(props.rounds_parrent))
+  rounds.value.forEach(ele => {
+    ele.player1.template_name = ele.player1.name
+    ele.player2.template_name = ele.player2.name
+  })
+  loaded_undefined.value = false
+}
+function splitFirstRound() {
+  if (!props.first_round) {
+    data.value = rounds.value
+    return
+  }
+  const first_round_matches = rounds.filter(e => e.type == props.first)
+  st_round = first_round_matches
+  const games = tmp_rounds.value[props.first - 2]
+  const winners = []
+  for (const [key, el] of Object.entries(games)) {
+    const match = st_round.find(e => e.player1.name == Object.keys(el)[0] || e.player2.name == Object.keys(el)[0])
+    if (match === undefined) {
+      console.log("Didn't find correct match. First round +  element: " + Object.keys(el))
+      return
+    }
+    const winner_team = el[match.player1.name] > el[match.player2.name] ? match.player1 : match.player2
+    const loser_team = el[match.player1.name] > el[match.player2.name] ? match.player2 : match.player1
+    const new_winner_team = structuredClone(winner_team)
+    new_winner_team.winner = null
+    winners.push(new_winner_team)
+    winner_team.winner = true
+    loser_team.winner = false
+    match.other_info = el[match.player1.name].toString() + ' - ' + el[match.player2.name].toString()
+  }
+  winners.sort((a, b) => Number(b.id) - Number(a.id))
+  winners.forEach((winner, i) => {
+    const placementString = (i + 1).toString() + '. Low Seed'
+    const new_match = rounds.value.find(el => el.player1.name == placementString || el.player2.name == placementString)
+    const correct_column = new_match.player1.name.includes(placementString) ? 'player1' : 'player2'
+    const template = new_match[correct_column].template_name
+    winner.template_name = template
+    new_match[correct_column] = winner
+  })
+  data.value = rounds.value.filter(e => e.type != props.first)
+}
+
+function resolvePlayoffs() {
+  const reversed_list = tmp_rounds.value.reverse()
+  reversed_list.forEach((ele, i) => {
+    for (const [key, el] of Object.entries(ele)) {
+      if (props.first_round && props.first == 7 - i) { continue }
+      const match = data.value.find(e => e.type == 7 - i && (Object.keys(el)[0] == e.player1.name || Object.keys(el)[0] == e.player2.name))
+      if (match === undefined) {
+        console.log("Didn't find correct match. type: " + 7 - i + ' element: ' + Object.keys(el))
+        return
+      }
+      if (el[match.player1.name] !== el[match.player2.name]) { // Not a tie
+        const winner = el[match.player1.name] > el[match.player2.name] ? match.player1 : match.player2
+        const loser = el[match.player1.name] > el[match.player2.name] ? match.player2 : match.player1
+        const new_winner = structuredClone(toRaw(winner))
+        const new_loser = structuredClone(toRaw(loser))
+        new_winner.winner = null
+        new_loser.winner = null
+        winner.winner = true
+        loser.winner = false
+        match.other_info = el[match.player1.name].toString() + ' - ' + el[match.player2.name].toString()
+        if (7 - i >= 4) { // Ignore Bronze and Finals
+          if (7 - i == 4) { // SemiFinals -> Winner needs to be assigned to Finals
+            const finals = rounds.value.find(ele => ele.type === 2)
+            const correct_column = finals.player1.name.includes(match.name) ? 'player1' : 'player2'
+            const template = finals[correct_column].template_name
+            new_winner.template_name = template
+            finals[correct_column] = new_winner
+          }
+          const new_match = rounds.value.find(ele => ele.id === match.next)
+          const n = (7 - i != 4) ? 'name' : 'loser_name'
+          const correct_column = new_match.player1.name.includes(match[n]) ? 'player1' : 'player2'
+          const template = new_match[correct_column].template_name
+
+          new_match[correct_column] = (7 - i == 4) && props.bronze ? new_loser : new_winner // SemiFinals -> Loser needs to be assigned to Bronze match
+          new_match[correct_column].template_name = template
+        }
+      }
+    }
+  }, this)
+}
+
+watch(() => props.load_ended, (loadReady) => {
+  if(!loadReady) return
+  loadRounds();
+  if(!loaded_undefined.value) {
+    resolveGames();
+    putTeamsPlayoffBracket();
+    splitFirstRound();
+    resolvePlayoffs();
+  }
+})
+
 </script>
+
 <style>
 .vtb-player {
     background-color: white;
