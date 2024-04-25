38112d456d9e90785acb3f7a58e0eb5f02166787
Totti-S <totti.sillanpaa@gmail.com>
2024-04-25 17:12:14 +0300

    More refactoring

--- frontend/src/components/Tournament.vue
+++ frontend/src/components/Tournament.vue
@@ -54,52 +54,48 @@ const props = defineProps({
 });
 
 const tmp_rounds = ref([{}, {}, {}, {}, {}, {}]);
-const rounds = ref([]);
+let rounds = [];
 const data = ref([]);
-const bracket_matches = ref([]);
 const st_round = ref([]);
 const loaded_undefined = ref(false);
 
 function putTeamsPlayoffBracket() {
-  if (props.non_default_seeds !== undefined) {
+  if (props.non_default_seeds !== undefined) { // Superweekend
     props.non_default_seeds.forEach(ele => {
-      if (ele[1] !== undefined) {
-        for (let matchIdx = 0; matchIdx < rounds.value.length; matchIdx++) {
-          const placementString = (ele[1]).toString() + '. Seed'
-          const match = rounds.value[matchIdx]
-          if (match.player1.name.startsWith(placementString)) {
-            match.player1.name = ele[0]
-            break
-          } else if (match.player2.name.startsWith(placementString)) {
-            match.player2.name = ele[0]
-            break
-          }
-        }
+      const [teamName, seed] = ele
+      const placementString = (seed).toString() + '. Seed'
+      const found = rounds.find(ele => 
+           ele.player1.name.startsWith(placementString)
+        || ele.player2.name.startsWith(placementString)
+      );
+      // We assume no errors: First undefined found is also first not-qualified-to-playoffs team
+      if (found !== undefined){
+        const correctBracket = found.player1.name.startsWith(placementString) 
+          ? found.player1 
+          : found.player2
+        correctBracket.name = teamName
       }
     })
-    return
-  }
-  props.bracket_placements.forEach((bracket, idx) => {
-    bracket.sort((a,b) => a.bracket_placement-b.bracket_placement)
-    for (let i = 0; i < bracket.length; i++) {
-      for (let matchIdx = 0; matchIdx < rounds.value.length; matchIdx++) {
+  } else {
+    props.bracket_placements.forEach((bracket, idx) => {
+      bracket.sort((a,b) => a.bracket_placement-b.bracket_placement)
+      for (let i = 0; i < bracket.length; i++) {
         const placementString = props.bracket_placements.length >= 2
           ? String.fromCharCode(65 + idx) + (i + 1).toString()
           : (i + 1).toString() + '. Seed';
-        const match = rounds.value[matchIdx]
-        if (match.player1.name === placementString) {
-          match.player1.name = bracket[i].current_abbreviation
-          break
-        } else if (match.player2.name === placementString) {
-          match.player2.name = bracket[i].current_abbreviation
-          break
-        }
+        const found = rounds.find((ele) => 
+          ele.player1.name === placementString || ele.player2.name === placementString
+        )
+        // We assume no errors: First undefined found is also first not-qualified-to-playoffs team
+        if (found === undefined) break 
+        const correctBracket = found.player1.name === placementString ? found.player1 : found.player2 
+        correctBracket.name = bracket[i].current_abbreviation
       }
-    }
-  })
+    })
+  }
 }
+
 function resolveGames() {
-  bracket_matches.value = props.played_games.filter(ele => !ele.post_season)
   const playoff_games = props.played_games.filter(ele => ele.post_season || ele.match_type >= 32)
   playoff_games.forEach(ele => {
     ele.match_type = ele.match_type >= 32 ? ele.match_type - 30 : ele.match_type
@@ -111,11 +107,16 @@ function resolveGames() {
         round[ele.seriers] = {}
         round[ele.seriers][ele.home_team.current_abbreviation] = 0
         round[ele.seriers][ele.away_team.current_abbreviation] = 0
+        round[ele.seriers]['tie'] = 0
+
       }
       if (ele.home_score_total < ele.away_score_total) {
         ++round[ele.seriers][ele.home_team.current_abbreviation]
       } else if (ele.home_score_total > ele.away_score_total) {
         ++round[ele.seriers][ele.away_team.current_abbreviation]
+      } else {
+        ++round[ele.seriers]['tie']
+
       }
     }
   })
@@ -125,8 +126,8 @@ function loadRounds() {
     loaded_undefined.value = true
     return
   }
-  rounds.value = structuredClone(toRaw(props.rounds_parrent))
-  rounds.value.forEach(ele => {
+  rounds = structuredClone(toRaw(props.rounds_parrent))
+  rounds.forEach(ele => {
     ele.player1.template_name = ele.player1.name
     ele.player2.template_name = ele.player2.name
   })
@@ -134,7 +135,7 @@ function loadRounds() {
 }
 function splitFirstRound() {
   if (!props.first_round) {
-    data.value = rounds.value
+    data.value = rounds
     return
   }
   const first_round_matches = rounds.filter(e => e.type == props.first)
@@ -155,17 +156,20 @@ function splitFirstRound() {
     winner_team.winner = true
     loser_team.winner = false
     match.other_info = el[match.player1.name].toString() + ' - ' + el[match.player2.name].toString()
+    if(el[match.tie]) {
+      match.other_info += ' (T: ' + el[match.tie] +')'; 
+    }
   }
   winners.sort((a, b) => Number(b.id) - Number(a.id))
   winners.forEach((winner, i) => {
     const placementString = (i + 1).toString() + '. Low Seed'
-    const new_match = rounds.value.find(el => el.player1.name == placementString || el.player2.name == placementString)
+    const new_match = rounds.find(el => el.player1.name == placementString || el.player2.name == placementString)
     const correct_column = new_match.player1.name.includes(placementString) ? 'player1' : 'player2'
     const template = new_match[correct_column].template_name
     winner.template_name = template
     new_match[correct_column] = winner
   })
-  data.value = rounds.value.filter(e => e.type != props.first)
+  data.value = rounds.filter(e => e.type != props.first)
 }
 
 function resolvePlayoffs() {
@@ -188,15 +192,18 @@ function resolvePlayoffs() {
         winner.winner = true
         loser.winner = false
         match.other_info = el[match.player1.name].toString() + ' - ' + el[match.player2.name].toString()
+        if(el[match.tie]) {
+          match.other_info += ' (Ties: ' + el[match.tie] +')'; 
+        }
         if (7 - i >= 4) { // Ignore Bronze and Finals
           if (7 - i == 4) { // SemiFinals -> Winner needs to be assigned to Finals
-            const finals = rounds.value.find(ele => ele.type === 2)
+            const finals = rounds.find(ele => ele.type === 2)
             const correct_column = finals.player1.name.includes(match.name) ? 'player1' : 'player2'
             const template = finals[correct_column].template_name
             new_winner.template_name = template
             finals[correct_column] = new_winner
           }
-          const new_match = rounds.value.find(ele => ele.id === match.next)
+          const new_match = rounds.find(ele => ele.id === match.next)
           const n = (7 - i != 4) ? 'name' : 'loser_name'
           const correct_column = new_match.player1.name.includes(match[n]) ? 'player1' : 'player2'
           const template = new_match[correct_column].template_name
