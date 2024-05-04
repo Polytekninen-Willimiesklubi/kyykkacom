b14d80f2a47c684431041a9fa20d7f5d7e79478c
Totti-S <totti.sillanpaa@gmail.com>
2024-05-05 01:30:22 +0300

    Removed layout upper layer from singel match page and uses correct layout

--- frontend/src/pages/ottelut/[id].vue
+++ frontend/src/pages/ottelut/[id].vue
@@ -1,125 +1,129 @@
 <template>
-  <v-layout class="pt-5">
-    <v-container grid-list-md>
-      <v-row>
-        <v-col>
-          <v-card v-if="dataReady">
-            <h3 class="text-md-left headline">
-              {{matchData.type_name}} kenttä <span v-if="matchData.field">{{matchData.field}}</span><span v-else>TBD</span>
-              <span style="float:right;">
-                {{ date.formatByString(date.date(matchData.match_time), 'yyyy-MM-dd HH:mm') }}
-              </span>
-            </h3>
-            <v-row>
-              <v-col justify="center" align="center" class="ml-5">
-                <figure>
-                  <v-img src="@/assets/kyykkalogo120px.png"/>
-                  <figcaption v-if="matchData.home_score_total">
-                    <br>
-                    <v-chip
-                      :color="`${getColor(matchData.home_score_total,
-                        matchData.away_score_total)} lighten-2`"
-                      :text="matchData.home_score_total"
-                    />
-                  </figcaption>
-                </figure>
-              </v-col>
-              <v-col justify="center" align="center">
-                <a :href="'/joukkueet/'+matchData.home_team.id">
-                  {{matchData.home_team.current_name}}
-                </a>
-              </v-col>
-              <v-col justify="center" align="center">
-                vs.
-              </v-col>
-              <v-col justify="center" align="center">
-                <a :href="'/joukkueet/'+matchData.away_team.id">
-                  {{matchData.away_team.current_name}}
-                </a>
-              </v-col>
-              <v-col justify="center" align="center" class="mr-5">
-                <figure>
-                  <v-img src="@/assets/kyykkalogo120px.png"/>
-                  <figcaption v-if="matchData.home_score_total">
-                    <br>
-                    <v-chip
-                      :color="`${getColor(matchData.away_score_total,
-                        matchData.home_score_total)} lighten-2`"
-                      :text="matchData.away_score_total"
-                    />
-                  </figcaption>
-                </figure>
-              </v-col>
-            </v-row>
-          </v-card>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col>
-          <v-card color="secondary">
-            <round 
-              v-if="dataReady" 
-              :matchData="matchData" 
-              roundNumber="1" 
-              teamSide="home"
-              :color="getColor(matchData.home_first_round_score,
-                matchData.away_first_round_score)"
-            />
-          </v-card>
-        </v-col>
-        <v-col>
-          <v-card color="secondary">
-            <round 
-              v-if="dataReady" 
-              :matchData="matchData" 
-              roundNumber="1" 
-              teamSide="away"
-              :color="getColor(matchData.away_first_round_score,
-                matchData.home_first_round_score)"
-            />
-          </v-card>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col>
-          <v-card color="secondary">
-            <round 
-              v-if="dataReady" 
-              :matchData="matchData" 
-              roundNumber="2" 
-              teamSide="home"
-              :color="getColor(matchData.home_second_round_score,
-                matchData.away_second_round_score)"
-            />
-          </v-card>
-        </v-col>
-        <v-col>
-          <v-card color="secondary">
-            <round 
-              v-if="dataReady" 
-              :matchData="matchData" 
-              roundNumber="2" 
-              teamSide="away"
-              :color="getColor(matchData.away_second_round_score,
-                matchData.home_second_round_score)"
-            />
-          </v-card>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col class="text-xs-center">
-          <v-btn 
-            text="vahvista"
-            @click="matchStore.validateClick()" 
-            color="error"
-            v-if="dataReady && authStore.isCaptain" 
+  <v-container grid-list-md>
+    <v-row>
+      <v-col>
+        <v-card v-if="dataReady">
+          <h3 class="text-md-left headline">
+            {{matchData.type_name}} kenttä <span v-if="matchData.field">{{matchData.field}}</span><span v-else>TBD</span>
+            <span style="float:right;">
+              {{ date.formatByString(date.date(matchData.match_time), 'yyyy-MM-dd HH:mm') }}
+            </span>
+          </h3>
+          <v-row>
+            <v-col justify="center" align="center" class="ml-5">
+              <figure>
+                <v-img src="@/assets/kyykkalogo120px.png"/>
+                <figcaption v-if="matchData.home_score_total">
+                  <br>
+                  <v-chip
+                    :color="`${getColor(matchData.home_score_total,
+                      matchData.away_score_total)} lighten-2`"
+                    :text="matchData.home_score_total"
+                  />
+                </figcaption>
+              </figure>
+            </v-col>
+            <v-col justify="center" align="center">
+              <a :href="'/joukkueet/'+matchData.home_team.id">
+                {{matchData.home_team.current_name}}
+              </a>
+            </v-col>
+            <v-col justify="center" align="center">
+              vs.
+            </v-col>
+            <v-col justify="center" align="center">
+              <a :href="'/joukkueet/'+matchData.away_team.id">
+                {{matchData.away_team.current_name}}
+              </a>
+            </v-col>
+            <v-col justify="center" align="center" class="mr-5">
+              <figure>
+                <v-img src="@/assets/kyykkalogo120px.png"/>
+                <figcaption v-if="matchData.home_score_total">
+                  <br>
+                  <v-chip
+                    :color="`${getColor(matchData.away_score_total,
+                      matchData.home_score_total)} lighten-2`"
+                    :text="matchData.away_score_total"
+                  />
+                </figcaption>
+              </figure>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-card color="secondary">
+          <round 
+            v-if="dataReady" 
+            :matchData="matchData" 
+            roundNumber="1" 
+            teamSide="home"
+            :color="getColor(matchData.home_first_round_score,
+              matchData.away_first_round_score)"
           />
-        </v-col>
-      </v-row>
-    </v-container>
-  </v-layout>
+        </v-card>
+      </v-col>
+      <v-col>
+        <v-card color="secondary">
+          <round 
+            v-if="dataReady" 
+            :matchData="matchData" 
+            roundNumber="1" 
+            teamSide="away"
+            :color="getColor(matchData.away_first_round_score,
+              matchData.home_first_round_score)"
+          />
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-card color="secondary">
+          <round 
+            v-if="dataReady" 
+            :matchData="matchData" 
+            roundNumber="2" 
+            teamSide="home"
+            :color="getColor(matchData.home_second_round_score,
+              matchData.away_second_round_score)"
+          />
+        </v-card>
+      </v-col>
+      <v-col>
+        <v-card color="secondary">
+          <round 
+            v-if="dataReady" 
+            :matchData="matchData" 
+            roundNumber="2" 
+            teamSide="away"
+            :color="getColor(matchData.away_second_round_score,
+              matchData.home_second_round_score)"
+          />
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col class="text-xs-center">
+        <v-btn 
+          text="vahvista"
+          @click="matchStore.validateClick()" 
+          color="error"
+          v-if="dataReady && authStore.isCaptain" 
+        />
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
-
+<route>
+{
+  meta: {
+    layout: "withoutSidebar"
+  }
+}
+</route>
 <script setup>
 import { useMatchStore } from '@/stores/match.store';
 import { useAuthStore } from '@/stores/auth.store';
