eb47d7534cba0d0b3058bfac63081609458cfa9c
Totti-S <totti.sillanpaa@gmail.com>
2024-07-26 19:03:35 +0300

    Initial commit to news box

---
+++ frontend/src/components/NewsBox.vue
@@ -0,0 +1,16 @@
+<template>
+	<v-card :title="title">
+
+	</v-card>
+</template>
+
+<script setup>
+
+const props = defineProps({
+	title: String,
+	text: String,
+})
+
+
+
+</script>
--- frontend/src/plugins/index.js
+++ frontend/src/plugins/index.js
@@ -8,10 +8,14 @@
 import vuetify from './vuetify'
 import pinia from '@/stores'
 import router from '@/router'
+import PrimeVue from 'primevue/config'
+import Editor from 'primevue/editor'
 
 export function registerPlugins (app) {
   app
     .use(vuetify)
     .use(router)
     .use(pinia)
+    .use(PrimeVue, { unstyled: true })
+    .component('Editor', Editor)
 }
