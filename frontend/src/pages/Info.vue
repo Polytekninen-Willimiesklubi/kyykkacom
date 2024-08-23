758ddb32578cca4596f67b21b63dac86250ea919
Totti-S <totti.sillanpaa@gmail.com>
2024-08-23 15:12:11 +0300

    Added pdf embed to info-page

--- frontend/src/pages/Info.vue
+++ frontend/src/pages/Info.vue
@@ -1,12 +1,48 @@
 <template>
   <v-row>
+    <v-col>
+      <span v-if="showAllPages">
+          {{ pageCount }} sivu(a)
+      </span>
+      <span v-else>
+        <button :disabled="page <= 1" @click="page--">❮</button>
+  
+        {{ page }} / {{ pageCount }}
+  
+        <button :disabled="page >= pageCount" @click="page++">❯</button>
+      </span>
+    </v-col>
+    <v-col>
+      <label >
+        <input 
+          v-model="showAllPages"
+          type="checkbox"
+          @input="() => {page = showAllPages ? null : 1}"
+        >
+        Näytä kaikki sivut
+      </label>
+    </v-col>
+  </v-row>
+  <v-row>
     <v-col align="center">
-      <a class="rules__link" href="/static/saannot_2024.pdf" download="saannot_2024.pdf">NKL Säännöt (pdf)</a>
+      <VuePdfEmbed 
+        source="saannot_2024.pdf" 
+        download="saannot_2024.pdf"
+        
+        @loaded="(obj) => {pageCount = obj.numPages; page = 1;console.log(pageCount)}"
+      />
     </v-col>
   </v-row>
 </template>
 
-<script setup></script>
+<script setup>
+import VuePdfEmbed from 'vue-pdf-embed';
+
+const pageCount = ref(null);
+const page = ref(null);
+const showAllPages = ref(false);
+
+</script>
 
 <style>
 .rules__link {
