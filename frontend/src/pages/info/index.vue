724f4e1199fbb934914de0d991d386ea6a0edebf
Totti-S <totti.sillanpaa@gmail.com>
2024-08-26 23:44:25 +0300

    Fixed and moved the embeded pdf to 'rules'-page

--- frontend/src/pages/info/index.vue
+++ frontend/src/pages/info/index.vue
@@ -1,53 +1,8 @@
 <template>
-  <v-row>
-    <v-col>
-      <span v-if="showAllPages">
-          {{ pageCount }} sivu(a)
-      </span>
-      <span v-else>
-        <button :disabled="page <= 1" @click="page--">❮</button>
-  
-        {{ page }} / {{ pageCount }}
-  
-        <button :disabled="page >= pageCount" @click="page++">❯</button>
-      </span>
-    </v-col>
-    <v-col>
-      <label >
-        <input 
-          v-model="showAllPages"
-          type="checkbox"
-          @input="() => {page = showAllPages ? null : 1}"
-        >
-        Näytä kaikki sivut
-      </label>
-    </v-col>
-  </v-row>
-  <v-row>
-    <v-col align="center">
-      <VuePdfEmbed 
-        source="saannot_2024.pdf" 
-        download="saannot_2024.pdf"
-        :page="page"
-        @loaded="(obj) => {pageCount = obj.numPages; page = 1;console.log(pageCount)}"
-      />
-    </v-col>
-  </v-row>
 </template>
 
 <script setup>
-import VuePdfEmbed from 'vue-pdf-embed';
-
-const pageCount = ref(null);
-const page = ref(null);
-const showAllPages = ref(false);
-
 </script>
 
 <style>
-.rules__link {
-  text-decoration: none;
-  color: red !important;
-}
-
 </style>
--- frontend/src/pages/info/saannot.vue
+++ frontend/src/pages/info/saannot.vue
@@ -1,17 +1,27 @@
 <template>
-    <VuePdfEmbed 
-        source="saannot_2024.pdf" 
-        download="saannot_2024.pdf"
-    />
+    <v-card class="mr-5" style="width: '900px';">
+        <VuePdfEmbed 
+            source="/saannot_2024.pdf" 
+            download="/saannot_2024.pdf"
+            width="900"
+            text-layer
+            :page="page"
+            @loaded="(obj) => {pageCount = obj.numPages; page = null;}"
+        />
+    </v-card>
 </template>
-
+<!-- 
 <route lang="yaml">
     meta:
         layout: "withoutSidebar"
-</route>
+</route> -->
 
 <script setup>
 import VuePdfEmbed from 'vue-pdf-embed';
+import 'vue-pdf-embed/dist/styles/textLayer.css'
+
+const pageCount = ref(null);
+const page = ref(null);
 
 </script>
 
