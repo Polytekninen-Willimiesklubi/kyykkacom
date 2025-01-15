c8672575b6357f451651be5173cf010a86753663
Totti-S <totti.sillanpaa@gmail.com>
2025-01-16 00:33:22 +0200

    Attempt to fix some of mobile issues

---
+++ frontend/src/pages/runkosarja.vue
@@ -0,0 +1,27 @@
+<template>
+  <div align="center">
+    <side-bar
+      title="Runkosarjatilanne"
+      :headers="headersDefaultSideBar"
+      :sort-by="[{key:'bracket_placement', order: 'asc'}, {key:'points_total', order: 'desc'}]"
+      :teams="teamStore.bracketedTeams"
+      :lines="navStore.playoffLines"
+      :boldingKeys="['P', 'points_total']"
+    />
+  </div>
+</template>
+
+<route lang="yaml">
+    meta:
+        layout: "withoutSidebar"
+</route>
+
+<script setup>
+import { useTeamsStore } from '@/stores/teams.store';
+import { useNavBarStore } from '@/stores/navbar.store';
+import { headersDefaultSideBar } from '@/stores/headers';
+
+const navStore = useNavBarStore(); 
+const teamStore = useTeamsStore();
+
+</script>
