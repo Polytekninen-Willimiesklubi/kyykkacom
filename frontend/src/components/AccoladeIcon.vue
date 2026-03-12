b2e1939835d87c9964f22fa8f1c913994ea62601
Totti <totti@sillanpaa@gmail.com>
2026-03-12 19:28:23 +0200

    HotFix: accolades not showing in the builded version

--- frontend/src/components/AccoladeIcon.vue
+++ frontend/src/components/AccoladeIcon.vue
@@ -1,7 +1,8 @@
 <template>
     <v-icon :size="size">
         <img 
-            :src="failed ? `src/assets/kyykkalogo120px.png` : `src/assets/accolades/${filename}`"
+            :src="imageIndex[props.filename]"
+            alt="src/assets/kyykkalogo120px.png"
         />
         <slot></slot>
     </v-icon>
@@ -18,6 +19,25 @@ const props = defineProps({
     },
 });
 const failed = ref(false);
+
+// FIXME Do better
+const imageIndex = {
+    "hauki.png": require("@/assets/accolades/hauki.png"),
+    "hopea.ico": require("@/assets/accolades/hopea.ico"),
+    "joulukuusen_kaataja.png": require("@/assets/accolades/joulukuusen_kaataja.png"),
+    "lakaisija.png": require("@/assets/accolades/lakaisija.png"),
+    "mestari.ico": require("@/assets/accolades/mestari.ico"),
+    "mvp.png": require("@/assets/accolades/mvp.png"),
+    "naismies_paras.svg": require("@/assets/accolades/naismies_paras.svg"),
+    "pronssi.ico": require("@/assets/accolades/pronssi.ico"),
+    "pudotus.ico": require("@/assets/accolades/pudotus.ico"),
+    "runkomestari.ico": require("@/assets/accolades/runkomestari.ico"),
+    "runkosarja_paras.ico": require("@/assets/accolades/runkosarja_paras.ico"),
+    "superweekend.ico": require("@/assets/accolades/superweekend.ico"),
+    "tulokas.ico": require("@/assets/accolades/tulokas.ico"),
+    "vuoden_kyykkaaja.svg": require("@/assets/accolades/vuoden_kyykkaaja.svg"),
+    "vuoden_tulokas.svg": require("@/assets/accolades/vuoden_tulokas.svg"),
+}
 
 if (props.filename === null || props.filename === undefined) {
     failed.value = true;
