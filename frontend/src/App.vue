12136eb3392a4bca8b83d1b5c899363a283d7fc4
Totti <totti@sillanpaa@gmail.com>
2026-03-12 19:28:23 +0200

    HotFix: accolades not showing in the builded version

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -7,9 +7,9 @@
  </template>
 
 <script setup>
-import {useNavBarStore} from '@/stores/navbar.store';
+import { useNavBarStore } from '@/stores/navbar.store';
 
-const store = useNavBarStore() 
+const store = useNavBarStore()
 
 if (!store.selectedSeason) {
   // Index starts at 1 : 2000
@@ -19,15 +19,13 @@ if (!store.selectedSeason) {
 </script>
 
 <style>
-
 #app {
-    background: url('./src/assets/kyykka_background.jpg') no-repeat center center fixed;
-    background-size: cover;
-    --v-theme-background: rgba(0,0,0,0) !important;
-  }
-  
-.v-application {
-  --v-theme-background: rgba(0,0,0,0) !important;
+  background: url('@/assets/kyykka_background.jpg') no-repeat center center fixed;
+  background-size: cover;
+  --v-theme-background: rgba(0, 0, 0, 0) !important;
 }
 
+.v-application {
+  --v-theme-background: rgba(0, 0, 0, 0) !important;
+}
 </style>
